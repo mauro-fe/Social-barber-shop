74d00adce9d20fd417448d22d90bb4523a7c3485
Mauro Felix <mauro.cardoso.1998@gmail.com>
2024-03-28 21:29:35 -0300

    Ajustando desktop

--- js/scripts.js
+++ js/scripts.js
@@ -4,53 +4,53 @@ $(function () {
     $('.mobile_action').click(function () {
         if (!$(this).hasClass('active')) {
             $(this).addClass('active');
-            $('.main_header_nav').animate({'left': '0px'}, 500);
+            $('.main_header_nav').animate({ 'left': '0px' }, 500);
         } else {
             $(this).removeClass('active');
-            $('.main_header_nav').animate({'left': '-100%'}, 500);
+            $('.main_header_nav').animate({ 'left': '-100%' }, 500);
         }
     });
 
     //HEADER
     $(window).scroll(function () {
-          
-          if($(this).scrollTop() > 0){
-              
-              if (!$('.main_header').hasClass('fixed')){
-                   $('.main_header').stop().addClass('fixed');
-              }
-          
-          }else{
-          
-              $('.main_header').removeClass('fixed');
-          
-          }
+
+        if ($(this).scrollTop() > 0) {
+
+            if (!$('.main_header').hasClass('fixed')) {
+                $('.main_header').stop().addClass('fixed');
+            }
+
+        } else {
+
+            $('.main_header').removeClass('fixed');
+
+        }
     });
 
     //Scroll Ancora
     var $doc = $('html, body');
-    $('.scrollSuave').click(function() {
+    $('.scrollSuave').click(function () {
         $doc.animate({
-            scrollTop:$($.attr(this,'href')).offset().top
-        }, 500);        
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
         return false;
     });
 
 
     //Magnific Popup
-    $('.galeria-barber').magnificPopup({ 
-      type: 'image',
-      delegate: 'a',
-      
-      gallery:{enabled:true},
-      callbacks: {
-        
-        buildControls: function() {
-         
-          this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+    $('.galeria-barber').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+
+        gallery: { enabled: true },
+        callbacks: {
+
+            buildControls: function () {
+
+                this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
+            }
+
         }
-        
-      }
     });
 
 });
