cc56e9b3228e14a1e3f3880aa34d422ccac9ea1b
Mauro Felix <mauro.cardoso.1998@gmail.com>
2024-04-02 12:36:40 -0300

    Ajustando tabela

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ $(function () {
 
     //HEADER
     $(window).scroll(function scroll() {
-
+        $('.main_header').removeClass('fixed')
         if ($(this).scrollTop() > 0) {
 
             if (!$('.main_header').hasClass('fixed')) {
@@ -29,25 +29,7 @@ $(function () {
     $('.scrollSuave').click(function () {
         $doc.animate({
             scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
+        }, 1000);
         return false;
     });
-
-
-    //Magnific Popup
-    $('.galeria-barber').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-
-        gallery: { enabled: true },
-        callbacks: {
-
-            buildControls: function () {
-
-                this.contentContainer.append(this.arrowLeft.add(this.arrowRight));
-            }
-
-        }
-    });
-
 });
