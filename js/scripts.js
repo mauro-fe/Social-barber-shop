cb579cb9fcaf91680264ffbc8c92c849e1721828
Mauro Felix <mauro.cardoso.1998@gmail.com>
2024-07-07 14:17:22 -0300

    ajustando detalhes

--- js/scripts.js
+++ js/scripts.js
@@ -17,32 +17,33 @@ $(function () {
     })
 
     //Scroll Ancora
-    var $doc = $('html, body');
+    let $doc = $('html, body');
     $('.scrollSuave').click(function () {
         $doc.animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 1000);
         return false;
     });
+
+    function horario() {
+        let diaAtual = new Date()
+        let horaAtual = diaAtual.getHours()
+        let headerWelcome = document.getElementsByClassName("header_welcome")[0]
+        let h2 = document.createElement("h2")
+
+        headerWelcome.appendChild(h2)
+        h2.classList.add("title")
+
+        if (horaAtual > 5 && horaAtual < 12) {
+            horaAtual = "Bom dia"
+        } else if (horaAtual < 18) {
+            horaAtual = "Boa tarde"
+        } else {
+            horaAtual = "Boa noite"
+        }
+        h2.innerHTML = `<b class="horario">${horaAtual}</b>, seja bem vindo à <b class="destaque">barbearia Social Barber Shop</b>`
+
+    }
+    horario()
 });
 
-function horario() {
-    let diaAtual = new Date()
-    let horaAtual = diaAtual.getHours()
-    let headerWelcome = document.getElementsByClassName("header_welcome")[0]
-    let h2 = document.createElement("h2")
-
-    headerWelcome.appendChild(h2)
-    h2.classList.add("title")
-
-    if (horaAtual > 5 && horaAtual < 12) {
-        horaAtual = "Bom dia"
-    } else if (horaAtual < 18) {
-        horaAtual = "Boa tarde"
-    } else {
-        horaAtual = "Boa noite"
-    }
-    h2.innerHTML = `<b class="horario">${horaAtual}</b>, seja bem vindo à <b class="destaque">barbearia Social Barber Shop</b>`
-
-}
-horario()
